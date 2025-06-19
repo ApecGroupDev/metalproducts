43ea765302894d0cb246f5efa2fec7265b58cb32
Mark Balajadia <markb.freelance@gmail.com>
2025-06-20 07:38:06 +0800

    MPC landing page hero section developed up to max-w

--- src/app/home/sections/hero.tsx
+++ src/app/home/sections/hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Hero: React.FC = () => {
   return (
     <div className="relative z-20">
-      <div className="relative flex flex-col items-center justify-center scrn-900:justify-normal bg-white border border-dashed border-b-green-600
+      <div className="relative flex flex-col items-center justify-center scrn-900:justify-normal scrn-1000:justify-center bg-white border border-dashed border-b-green-600
       h-140
       scrn-450:h-148
       scrn-500:h-160
@@ -13,27 +13,29 @@ const Hero: React.FC = () => {
       scrn-700:h-224
       scrn-800:h-240
       scrn-900:h-148
-      scrn-1000:min-h-screen
-      scrn-1150:h-168
-      scrn-1200:h-176
-      scrn-1300:h-188
-      scrn-1400:h-198
-      scrn-1500:h-216
-      scrn-1600:h-228
-      scrn-1700:h-248
-      scrn-1800:h-260
-      scrn-1900:min-h-screen
-      scrn-2000:h-280
-      scrn-2100:h-288
-      scrn-2200:h-304
-      scrn-2300:h-308
-      scrn-2350:h-320
-      scrn-2400:h-324
-      scrn-2500:h-339"
+      scrn-1000:h-180
+      scrn-1150:h-188
+      scrn-1300:h-195
+      scrn-1350:h-208
+      scrn-1400:h-216
+      scrn-1450:h-228
+      scrn-1500:h-240
+      scrn-1650:h-272
+      scrn-1750:h-296
+      scrn-2000:h-312
+      scrn-2150:h-324
+      scrn-2350:h-360
+      scrn-2500:h-384"
       >
+        {/* Hero BG */}
         <div className="absolute inset-0 z-10 hidden scrn-900:block
         scrn-900:-mt-[4.5rem]
-        scrn-1900:-mt-40"
+        scrn-1200:-mt-24
+        scrn-1400:-mt-28
+        scrn-1600:-mt-32
+        scrn-1750:-mt-36
+        scrn-1900:-mt-40
+        scrn-2100:-mt-48"
         >
           <Image
             src="/images/backgrounds/home/Home_Dev_1.png"
@@ -45,7 +47,7 @@ const Hero: React.FC = () => {
           />
         </div>
 
-      {/* Mobile only hero image */}
+        {/* Mobile only hero image */}
         <div className="absolute inset-0 z-10 scrn-900:hidden">
           <Image
             src="/images/backgrounds/home/Hero_Homepage_Mobile.png"
@@ -57,6 +59,7 @@ const Hero: React.FC = () => {
           />
         </div>
 
+        {/* MAIN HERO TANK */}
         <div className="absolute z-50 w-full flex justify-center 
         mt-72
         scrn-450:mt-80
@@ -69,7 +72,14 @@ const Hero: React.FC = () => {
         scrn-800:mt-144
         scrn-850:mt-164
         scrn-900:mt-80
-        scrn-1900:mt-164"
+        scrn-1000:mt-80
+        scrn-1050:mt-96
+        scrn-1100:mt-120
+        scrn-1150:mt-128
+        scrn-1500:mt-160
+        scrn-1900:mt-164
+        scrn-2050:mt-180
+        scrn-2150:mt-208"
         >
           <Image
             src="/images/backgrounds/home/Hero_Tank.png"
@@ -84,7 +94,21 @@ const Hero: React.FC = () => {
         </div>
 
         {/*  Text Section */}
-        <div className="text-center z-40 mt-8 scrn-650:mt-20 scrn-900:mt-40">
+        <div className="text-center z-40 
+        mt-8 
+        scrn-650:mt-20 
+        scrn-900:mt-40 
+        scrn-1000:mt-0 
+        scrn-1050:mt-12
+        scrn-1100:mt-10
+        scrn-1150:mt-24
+        scrn-1300:mt-16
+        scrn-1500:mt-24
+        scrn-1700:mt-16
+        scrn-1900:mt-8
+        scrn-2000:mt-0
+        scrn-2100:mt-12"
+        >
           <p
             className="font-bold opacity-90
             text-4xl
@@ -92,7 +116,10 @@ const Hero: React.FC = () => {
             scrn-600:text-7xl
             scrn-700:text-8xl
             scrn-900:text-7xl
-            scrn-1900:text-9xl"
+            scrn-1100:text-8xl
+            scrn-1500:text-9xl
+            scrn-1900:text-9xl
+            scrn-2100:text-10xl"
             style={{
               color: 'rgba(255, 255, 255, 255)',
               textShadow: `
