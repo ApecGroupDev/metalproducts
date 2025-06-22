b6207a51093e7d24940347ee8decb4bb71ee82de
Mark Balajadia <markb.freelance@gmail.com>
2025-06-22 11:39:19 +0800

    layout tsx update

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -3,9 +3,9 @@ import Header from '@/components/header';
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-white bg-cover bg-top bg-no-repeat min-h-screen">
+    <div className='bg-white bg-cover bg-top bg-no-repeat min-h-screen'>
       <Header />
-      <div className="w-full">{children}</div>
+      <div className='w-full'>{children}</div>
       <Footer />
     </div>
   );
