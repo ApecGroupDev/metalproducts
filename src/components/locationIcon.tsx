aa50c5490ba061911cfd9649529fed0f207cefe5
Mark Balajadia <markb.freelance@gmail.com>
2025-06-22 11:35:37 +0800

    locationIcon & scrollToTop tsx update

--- src/components/locationIcon.tsx
+++ src/components/locationIcon.tsx
@@ -2,9 +2,9 @@ import { TfiLocationPin } from "react-icons/tfi";
 
 const LocationIcon = ({ text = "", size = 24 }) => {
   return (
-    <div className="flex items-center scrn-800:font-medium scrn-1000:-ms-1 scrn-1500:ms-0">
+    <div className='flex items-center scrn-800:font-medium scrn-1000:-ms-1 scrn-1500:ms-0'>
       <TfiLocationPin size={size} />
-      {text && <span className="ms-1">{text}</span>}
+      {text && <span className='ms-1'>{text}</span>}
     </div>
   );
 };
--- src/components/scrollToTop.tsx
+++ src/components/scrollToTop.tsx
@@ -29,10 +29,7 @@ const ScrollToTopButton = () => {
 
   return (
     isVisible && (
-      <button
-        onClick={scrollToTop}
-        className="fixed bottom-3 right-3 bg-[#c62931] text-white text-2xl rounded-full p-3 shadow-lg hover:bg-red-600 focus:outline-none transition-all duration-200 z-50 border border-white"
-      >
+      <button onClick={scrollToTop} className='fixed bottom-3 right-3 bg-[#c62931] text-white text-2xl rounded-full p-3 shadow-lg hover:bg-red-600 focus:outline-none transition-all duration-200 z-50 border border-white'>
         ↑
       </button>
     )
