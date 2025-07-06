b0294132d2fbf49cf2107c95172710dc3f7c1499
Mark Balajadia <markb.freelance@gmail.com>
2025-07-06 18:46:44 +0800

    above ground tanks responsiveness update

--- src/app/above-ground-tank/sections/hero.tsx
+++ src/app/above-ground-tank/sections/hero.tsx
@@ -5,7 +5,31 @@ import AboveGroundTankAnimation from '@/components/aboveGroundTankAnimation';
 const Hero: React.FC = () => {
   return (
     <div className="relative z-20">
-      <div className="relative flex flex-col items-center justify-center scrn-900:justify-normal scrn-1000:justify-center bg-[#efefef] border border-dashed border-b-green-600 h-140 scrn-450:h-148 scrn-500:h-160 scrn-550:h-168 scrn-600:h-208 scrn-700:h-224 scrn-800:h-240 scrn-900:h-148 scrn-1000:h-180 scrn-1150:h-188 scrn-1300:h-195 scrn-1350:h-208 scrn-1400:h-216 scrn-1450:h-228 scrn-1500:h-240 scrn-1650:h-272 scrn-1750:h-296 scrn-2000:h-312 scrn-2150:h-324 scrn-2350:h-360 scrn-2500:h-384">
+      <div className="relative flex flex-col items-center justify-center scrn-900:justify-normal scrn-1000:justify-center bg-[#efefef] border border-dashed border-b-green-600 h-140 
+      scrn-450:h-148 
+      scrn-500:h-160 
+      scrn-550:h-180 
+      scrn-600:h-208 
+      scrn-700:h-224 
+      scrn-750:h-232
+      scrn-800:h-248
+      scrn-900:h-160
+      scrn-1000:h-180 
+      scrn-1100:h-195
+      scrn-1300:h-224
+      scrn-1350:h-228 
+      scrn-1400:h-232
+      scrn-1450:h-236
+      scrn-1500:h-260
+      scrn-1650:h-272 
+      scrn-1750:h-312 
+      scrn-2000:h-316 
+      scrn-2100:h-320
+      scrn-2150:h-324
+      scrn-2200:h-339
+      scrn-2300:h-368
+      scrn-2400:h-384
+      scrn-2450:h-416">
         {/* Hero BG */}
         <div className="absolute inset-0 z-10 hidden scrn-900:block scrn-900:-mt-[4.5rem] scrn-1200:-mt-24 scrn-1400:-mt-28 scrn-1600:-mt-32 scrn-1750:-mt-36 scrn-1900:-mt-40 scrn-2100:-mt-48">
           <Image
@@ -21,7 +45,7 @@ const Hero: React.FC = () => {
         {/* Mobile only hero image */}
         <div className="absolute inset-0 z-10 scrn-900:hidden">
           <Image
-            src="/images/backgrounds/home/002.png"
+            src="/images/backgrounds/home/Hero_Homepage_Mobile.png"
             alt="Background"
             style={{ objectFit: 'contain' }}
             width={2786}
@@ -31,10 +55,10 @@ const Hero: React.FC = () => {
         </div>
 
         {/* MAIN HERO TANK */}
-        <AboveGroundTankAnimation/>
+        <AboveGroundTankAnimation />
 
         {/*  Text Section */}
-        <div className="text-center z-40 mt-8 scrn-650:mt-20 scrn-900:mt-40 scrn-1000:mt-0 scrn-1050:mt-12 scrn-1100:mt-10 scrn-1150:mt-24 scrn-1300:mt-16 scrn-1500:mt-24 scrn-1700:mt-16 scrn-1900:-mt-36 scrn-2000:mt-0 scrn-2100:-mt-8 scrn-2150:mt-12">
+        <div className="text-center z-40 mt-8 scrn-650:mt-20 scrn-900:mt-40 scrn-1000:mt-0 scrn-1050:mt-12 scrn-1100:mt-10 scrn-1300:mt-16 scrn-1500:mt-16 scrn-1700:mt-16 scrn-1900:-mt-36 scrn-2000:mt-0 scrn-2100:-mt-8 scrn-2150:mt-0">
           <p
             className="font-bold opacity-90 text-4xl scrn-400:text-6xl scrn-600:text-7xl scrn-700:text-8xl scrn-900:text-7xl scrn-1100:text-8xl scrn-1500:text-9xl scrn-1900:text-9xl scrn-2100:text-10xl"
             style={{
@@ -48,7 +72,7 @@ const Hero: React.FC = () => {
           >
             ABOVE <br />
             GROUND <br />
-            TANK
+            TANKS
           </p>
         </div>
       </div>
