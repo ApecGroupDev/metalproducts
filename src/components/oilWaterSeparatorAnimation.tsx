85e8c4f1a3c9e6f6615b6ea96216f06d87965d41
Mark Balajadia <markb.freelance@gmail.com>
2025-07-12 10:58:24 +0800

    responsiveness oil and water separator

--- src/components/oilWaterSeparatorAnimation.tsx
+++ src/components/oilWaterSeparatorAnimation.tsx
@@ -19,22 +19,29 @@ const OilWaterSeparatorAnimation: React.FC = () => {
   const src = `/images/backgrounds/resources/oil_and_water_separator/OilSeprator${paddedFrame}.png`;
 
   return (
-    <div className='absolute z-50 w-full flex justify-center 
-    mt-48 
-    scrn-500:mt-52  
-    scrn-600:mt-56 
-    scrn-700:mt-72
-    scrn-800:mt-80
-    scrn-900:mt-32
-    scrn-1000:mt-36
-    scrn-1050:mt-48
-    scrn-1100:mt-52
-    scrn-1200:mt-60
-    scrn-1300:mt-64
-    scrn-1500:mt-72
-    scrn-1900:mt-128
-    scrn-2000:mt-60
-    scrn-2300:mt-72'>
+    <div className='absolute z-50 w-full flex justify-center me-4 scrn-1900:me-12 scrn-2100:me-20
+    mt-64
+    scrn-400:mt-72
+    scrn-450:mt-80
+    scrn-500:mt-96
+    scrn-650:mt-120
+    scrn-700:mt-128
+    scrn-800:mt-140
+    scrn-850:mt-152
+    scrn-900:mt-72
+    scrn-1000:mt-80
+    scrn-1050:mt-116
+    scrn-1100:mt-120
+    scrn-1200:mt-128
+    scrn-1300:mt-132
+    scrn-1500:mt-160
+    scrn-1700:mt-172
+    scrn-1900:mt-152
+    scrn-2000:mt-172
+    scrn-2100:mt-180
+    scrn-2150:mt-192
+    scrn-2500:mt-208
+    '>
       <img
         src={src}
         alt={`Frame ${paddedFrame}`}
