a8dcec9f6f0dd791a2bbf8b8945173b1516ed917
Mark Balajadia <markb.freelance@gmail.com>
2025-08-05 03:04:00 +0800

    MPC /home to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import { redirect } from 'next/navigation';
+import React from 'react';
+import Hero from '@/app/home/sections/hero';
+import History from '@/app/home/sections/history';
+import Resources from '@/app/home/sections/resources';
+import HomePageLayout from './layouts/homepageLayout';
 
-export default function Page() {
-  redirect('/home'); // Redirect to the Home page
-  return null; // This won't render since the redirect happens immediately
-}
+const Home: React.FC = () => {
+  return (
+    <HomePageLayout>
+      <div className='flex flex-col'>
+        {/* Sections */}
+        <Hero />
+        <History />
+        <Resources />
+      </div>
+    </HomePageLayout>
+  );
+};
+
+export default Home;
