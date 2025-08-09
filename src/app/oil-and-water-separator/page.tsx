bb82cb229c93411f8bb0cb51fe0a4c02886d47ef
Mark Balajadia <markb.freelance@gmail.com>
2025-08-10 03:18:37 +0800

    oil-and-water-separator metadata update, set to canonical

--- src/app/oil-and-water-separator/page.tsx
+++ src/app/oil-and-water-separator/page.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import Hero from './sections/hero';
 import Main from './sections/main';
 import PDFs from './sections/pdfs';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Oil-Water Separators Built to Last – USA Made",
+  description: "Discover premium oil-water separators made in the USA, ideal for industrial and commercial applications. Ships nationwide.",
+  alternates: {
+    canonical: 'https://www.metalproductsusa.com/oil-and-water-separator',
+  },
+};
 
 const Home: React.FC = () => {
   return (
