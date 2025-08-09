fdae9f353a9354717d6c01544fed9de256b7cb08
Mark Balajadia <markb.freelance@gmail.com>
2025-08-09 12:56:37 +0800

    about page metadata integration, set to canonical

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Hero from './sections/hero';
 import Timeline from './sections/timeline';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "About Metal Products USA – Tank Manufacturers",
+  description: "Learn about Metal Products USA, trusted tank manufacturers with decades of experience in custom industrial tank solutions.",
+  alternates: {
+    canonical: 'https://www.metalproductsusa.com/about',
+  },
+};
 
 const Home: React.FC = () => {
   return (
     <div className='flex flex-col'>
       {/* Sections */}
       <Hero />
-      <Timeline/>
+      <Timeline />
     </div>
   );
 };
