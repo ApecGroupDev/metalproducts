aca28e73fd6455db1f436edc1b713b640f47c12d
Mark Balajadia <markb.freelance@gmail.com>
2025-08-10 09:25:28 +0800

    underground-tanks metadata and set to canonical

--- src/app/underground-tanks/page.tsx
+++ src/app/underground-tanks/page.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import Hero from '@/app/underground-tanks/sections/hero';
 import Main from './sections/main';
 import PDFs from './sections/pdfs';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Underground Storage Tanks Built for Industry",
+  description: "Get heavy-duty underground storage tanks that meet EPA standards and perform in the toughest environments.",
+  alternates: {
+    canonical: 'https://www.metalproductsusa.com/underground-tanks',
+  },
+};
 
 const Home: React.FC = () => {
   return (
