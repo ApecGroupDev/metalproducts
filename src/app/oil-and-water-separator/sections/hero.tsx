9c1d373bfc14a3ea1da8788259fce1bd0a068b17
Mark Balajadia <markb.freelance@gmail.com>
2025-08-20 03:10:52 +0800

    oil-and-water-separator updated tsx

--- src/app/oil-and-water-separator/sections/hero.tsx
+++ src/app/oil-and-water-separator/sections/hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import OilWaterSeparatorAnimation from '@/components/oilWaterSeparatorAnimation';
 
 const Hero: React.FC = () => {
   return (
@@ -56,7 +55,37 @@ const Hero: React.FC = () => {
         </div>
 
         {/* MAIN HERO TANK */}
-        <OilWaterSeparatorAnimation />
+        <div className='absolute z-50 w-full flex justify-center me-4 scrn-1900:me-12 scrn-2100:me-20
+              mt-64
+              scrn-400:mt-72
+              scrn-450:mt-80
+              scrn-500:mt-96
+              scrn-650:mt-120
+              scrn-700:mt-128
+              scrn-800:mt-140
+              scrn-850:mt-152
+              scrn-900:mt-72
+              scrn-1000:mt-80
+              scrn-1050:mt-116
+              scrn-1100:mt-120
+              scrn-1200:mt-128
+              scrn-1300:mt-132
+              scrn-1500:mt-160
+              scrn-1700:mt-172
+              scrn-1900:mt-152
+              scrn-2000:mt-172
+              scrn-2100:mt-180
+              scrn-2150:mt-192
+              scrn-2500:mt-208
+            '>
+          <Image
+            src='/images/backgrounds/resources/oil_and_water_separator/Main139.png'
+            alt="Oil and Water Separator"
+            width={1920}
+            height={300}
+            className='scrn-900:w-4/6 scrn-1000:w-6/7 scrn-1900:w-5/7 scale-90'
+          />
+        </div>
 
         {/*  Text Section */}
         <div className="text-center z-40 mt-8 scrn-650:mt-20 scrn-900:mt-40 scrn-1000:mt-0 scrn-1050:mt-12 scrn-1100:mt-10 scrn-1300:mt-16 scrn-1500:mt-16 scrn-1700:mt-16 scrn-1900:-mt-36 scrn-2000:mt-0 scrn-2100:-mt-8 scrn-2150:mt-0">
