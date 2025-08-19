8fc94a2b05a4592c21c5d31057e2fd2d06f2c703
Mark Balajadia <markb.freelance@gmail.com>
2025-08-20 06:52:28 +0800

    underground-tanks updated tsx

--- src/app/underground-tanks/sections/hero.tsx
+++ src/app/underground-tanks/sections/hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import UndergroundTankAnimation from '@/components/undergroundTankAnimation';
 
 const Hero: React.FC = () => {
   return (
@@ -56,7 +55,31 @@ const Hero: React.FC = () => {
         </div>
 
         {/* MAIN HERO TANK */}
-        <UndergroundTankAnimation />
+        <div className='absolute z-50 w-full flex justify-center 
+              mt-48 
+              scrn-500:mt-52  
+              scrn-600:mt-56 
+              scrn-700:mt-72
+              scrn-800:mt-80
+              scrn-900:mt-32
+              scrn-1000:mt-36
+              scrn-1050:mt-48
+              scrn-1100:mt-52
+              scrn-1200:mt-60
+              scrn-1300:mt-64
+              scrn-1500:mt-72
+              scrn-1900:mt-40
+              scrn-2000:mt-44 
+              scrn-2200:mt-48'
+        >
+          <Image
+            src='/images/backgrounds/resources/underground_tanks/Home_Dev_1098.png'
+            alt="Underground Tank"
+            width={1920}
+            height={300}
+            className='scrn-900:w-4/6 scrn-1000:w-3/5 scrn-1900:w-2/3'
+          />
+        </div>
 
         {/*  Text Section */}
         <div className='text-center z-40 mt-8 scrn-650:mt-20 scrn-900:mt-40 scrn-1000:mt-0 scrn-1050:mt-12 scrn-1100:mt-0 scrn-1300:mt-12 scrn-1500:-mt-16 scrn-1700:-mt-24 scrn-1900:-mt-40'>
