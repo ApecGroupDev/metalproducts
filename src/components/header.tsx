bddcae0960c1291412de4bc27ceb13de0fadafcd
Mark Balajadia <markb.freelance@gmail.com>
2025-08-22 11:44:02 +0800

    components/header className update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -60,11 +60,11 @@ const Header: React.FC = () => {
           aria-label={isOpen ? "Close menu" : "Open menu"}
         >
           {isOpen ? (
-            <span className="text-5xl">×</span>
+            <span className='text-5xl'>×</span>
           ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
+              className='h-8 w-8'
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
