f57bc8c92f2f8c6d792d5e60da12eb1c55ead9df
Mark Balajadia <markb.freelance@gmail.com>
2025-11-09 03:58:32 +0800

    undeground-tanks page tsx update

--- src/app/underground-tanks/page.tsx
+++ src/app/underground-tanks/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import Hero from '@/app/underground-tanks/sections/hero';
-import Main from './sections/main';
-import PDFs from './sections/pdfs';
 import { Metadata } from 'next';
+import Hero_Underground from './sections/hero_underground';
+import Choose_Underground from './sections/choose_underground';
+import Features_Underground from './sections/features_underground';
+import Benefits_Underground from './sections/benefits_underground';
+import Concerns_Underground from './sections/concerns_underground';
+import Trust_Underground from './sections/trust_underground';
+import PDFs_Underground from './sections/pdfs_underground';
+import CTA from '../contact/sections/cta';
+import FAQ_Underground from './sections/faq_underground';
 
 export const metadata: Metadata = {
   title: "Underground Storage Tanks Built for Industry",
@@ -17,9 +23,15 @@ const Home: React.FC = () => {
   return (
     <div className='flex flex-col'>
       {/* Sections */}
-      <Hero />
-      <Main />
-      <PDFs />
+      <Hero_Underground />
+      <Choose_Underground />
+      <Features_Underground />
+      <Benefits_Underground />
+      <CTA />
+      <PDFs_Underground />
+      <Concerns_Underground />
+      <Trust_Underground />
+      <FAQ_Underground />
     </div>
   );
 };
