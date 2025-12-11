fb28d0c8aeb009d01331dcd50243f1da1f27b8ac
Mark Balajadia <markb.freelance@gmail.com>
2025-12-11 08:49:09 +0800

    removed tesh push content coding

--- src/app/underground-tanks/sections/trust_underground.tsx
+++ src/app/underground-tanks/sections/trust_underground.tsx
@@ -55,10 +55,6 @@ const Trust_Underground: React.FC = () => {
             </ul>
           </motion.div>
 
-          {/* test push content */}
-
-          {/* 2nd test push content */}
-
           {/* Right Side – Case Study Highlight */}
           <motion.div
             initial={{ opacity: 0, x: 30 }}
