3a8679a5ae0392d46eec0c31060c317fb34df9b7
Mark Balajadia <markb.freelance@gmail.com>
2026-03-21 03:52:55 +0800

    last push fix error - unused variable issue

--- src/app/oilfield-tanks-resources/sections/features_OTR.tsx
+++ src/app/oilfield-tanks-resources/sections/features_OTR.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { CheckCircleIcon as CheckCircle } from 'lucide-react';
 
